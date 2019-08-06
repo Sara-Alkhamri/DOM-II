bbc1d39866bb8f27ea38fa8b6387f94c6e67743a
Sara Alkhamri <s.alkhamri@gmail.com>
2019-08-06 12:58:51 -0700

    created mouseover and mouseout for nav-links

--- js/index.js
+++ js/index.js
@@ -1,1 +1,54 @@
 // Your code goes here
+
+
+// mouseover
+const nav = document.querySelectorAll(".nav-link");
+for (let i = 0; i < nav.length; i++) {
+    nav[i].addEventListener("mouseover", (e) => {
+        nav[i].style.color = "#17A2B8";
+        nav[i].style.fontSize = "2rem";
+    })
+}
+
+//mouseout
+
+for (let i = 0; i < nav.length; i++) {
+    nav[i].addEventListener("mouseout", (e) => {
+        nav[i].style.color = "black";
+        nav[i].style.fontSize = " 1.5rem";
+    }) 
+}
+
+
+//  keydown
+
+
+
+//  wheel
+
+
+
+
+//  drag / drop
+
+
+
+//  load
+
+
+
+//  focus
+
+
+
+//  resize
+
+
+
+//  scroll
+
+
+//  select
+
+
+//  dblclick
